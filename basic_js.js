02b53e44707edb90d39d73383cb7e5b6869fe1eb
Sahil zagade <148172621+Samy-20@users.noreply.github.com>
2024-10-06 14:20:52 +0530

    Create basic_js.js

---
+++ basic_js.js
@@ -0,0 +1,63 @@
+// (1) :- basic of javascript
+
+// var is used to denote a variable in JS
+var x = 21;
+// x, y is a name of that variable
+var y = "hello"
+
+// console.log() -> is used to print value
+console.log(y);
+console.log("hello");
+
+// (2)  dataypes in JS
+// 1] numbers -> (e.g. 10, 3.14)
+// 2]String -> (e.g. "hello", 'world')
+// 3]Booleans (e.g. true, false)
+// 4]Arrays (e.g. [1, 2, 3])
+// 5]Objects (e.g. { name: "John", age: 30 })
+
+var n = 10; // x is a number
+console.log(n);
+
+var s = "hello"; // y is a string
+console.log(s);
+
+var a = [1, 2, 3]; // z is an array
+console.log(a);
+
+// // Rules of variable naming 
+// Variable names can only contain letters, digits, underscores, and dollar signs.
+// Variable names cannot start with a digit.
+// Variable names are case-sensitive.
+
+var x = 10;
+var y = 20;
+var z = x + y; // z is 30
+
+var str1 = "hello";
+var str2 = "world";
+var str3 = str1 + " " + str2; // str3 is "hello world"
+
+// In JavaScript, there are two main types of data: primitives and objects.
+
+let x1 = 10;
+console.log(x1);
+x1 = 20; // x is now 20
+console.log(x1);
+
+let obj = { name: "John", age: 30 };
+console.log(obj);
+obj.age = 31; // the age property of obj is now 31
+console.log(obj);
+
+// // Operators and Expressions
+// Arithmetic operators (e.g. +, -, *, /, %)
+// Comparison operators (e.g. >, <, >=, <=, ==, !=)
+// Logical operators (e.g. &&, ||, !)
+// Assignment operators (e.g. =, +=, -=, *=, /=)
+// Conditional (ternary) operator (e.g. ?:)
+
+let m = 10 + 5 * 3; // x is 25
+console.log(m);
+
+
