d4ec947d86ca01fdc8f56d58e5a98f4b8eea57ae
Sahil zagade <148172621+Samy-20@users.noreply.github.com>
2024-12-27 08:56:57 +0530

    Create for_loop.js

---
+++ Loop/for_loop.js
@@ -0,0 +1,20 @@
+for (let i=0;  i<=6; i++){
+    console.log("for loop");
+}
+
+console.log("");
+
+let j = 0;
+while(j<=10){
+    console.log("while loop");
+    j++; 
+}
+
+console.log("");
+
+let k = 0;
+do{
+    console.log("do while");
+    k++;
+
+}while(k<=10);
