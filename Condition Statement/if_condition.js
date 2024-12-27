80a51cfa5eadff7ae4027ecb754e099c5472da76
Sahil zagade <148172621+Samy-20@users.noreply.github.com>
2024-12-27 08:54:04 +0530

    Create if_condition.js

---
+++ Condition Statement/if_condition.js
@@ -0,0 +1,32 @@
+
+// console.log("switch cases");
+// const val = "yellow";
+
+// switch (val) {
+//     case "orange":
+//         console.log("chouce is orange");
+//         break;
+//     case "yellow":
+//         console.log("yellow");
+//         break;
+//     default:
+//         console.log("no any present");
+//         break;
+
+// }
+// console.log("");
+
+// // popup on website
+// alert("hello guys");
+// // show and take input
+// prompt("name is");
+
+let age = prompt("enter you age:");
+
+if(age >= 18){
+    console.log("You are elligible to gave exam");
+}
+else{
+    console.log("You are not elligible to gave exam");
+}
+ 
