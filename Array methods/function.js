5fe2f423f693d9432c9cb9d81544cdc18cc6ef2c
Sahil zagade <148172621+Samy-20@users.noreply.github.com>
2025-01-07 15:52:41 +0530

    Create function.js

---
+++ Array methods/function.js
@@ -0,0 +1,13 @@
+let a = "abdc";
+console.log(a.toUpperCase()); // toUpperCase :-> is an function.
+
+function myFunction() {
+    console.log("hello World");
+    console.log("hello dear");
+}
+
+let n = prompt("Enter the number");
+
+for (let i = 0; i < n; i++) {
+    myFunction(); // call function
+}
